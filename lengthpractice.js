ec23eb3ea4aca61b7318950943c04d7dc469a95e
lakshmi priya <lakshmipriyaark@gmail.com>
2025-10-06 16:42:45 -0500

    length practice added

---
+++ lengthpractice.js
@@ -0,0 +1,17 @@
+
+// Starter Code (Pre-filled):
+// Predefined strings
+let stringOne = "Coding Bootcamp";
+let stringTwo = "JavaScript";
+let stringThree = "";
+
+// Your task: Assign the length of each string to the corresponding variable
+let lengthOne = stringOne.length;   // Length of stringOne
+let lengthTwo = stringTwo.length;   // Length of stringTwo
+let lengthThree = stringThree.length; // Length of stringThree
+
+// Your code here
+
+console.log("The length of stringOne is : ",lengthOne);
+console.log("The length of stringTwo is : ",lengthTwo);
+console.log("The length of stringThree is : ",lengthThree);
